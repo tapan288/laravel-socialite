29b597f7a38f1611b26612fb50ace4e7b2393e35
Tapan Sharma <tapansharma69@gmail.com>
2024-01-11 22:57:15 +0545

    update user model and implement login flow

--- app/Http/Controllers/Socialite/AuthCallbackController.php
+++ app/Http/Controllers/Socialite/AuthCallbackController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers\Socialite;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthCallbackController extends Controller
 {
     public function __invoke()
     {
-        dd(Socialite::driver('twitter')->user());
+        $user = Socialite::driver('twitter')->user();
+
+        auth()->login(User::firstOrCreate([
+            'x_id' => $user->getId(),
+        ], [
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+        ]));
+
+        return redirect(RouteServiceProvider::HOME);
     }
 }
