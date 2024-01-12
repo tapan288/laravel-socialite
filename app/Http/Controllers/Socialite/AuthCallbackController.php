42586139b571350c251c2cf0adb19207211823fa
Tapan Sharma <tapansharma69@gmail.com>
2024-01-12 22:16:53 +0545

    triggering email verification

--- app/Http/Controllers/Socialite/AuthCallbackController.php
+++ app/Http/Controllers/Socialite/AuthCallbackController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Socialite;
 
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use App\Services\SocialiteService;
 use Illuminate\Http\Request;
+use App\Services\SocialiteService;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Registered;
+use App\Providers\RouteServiceProvider;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthCallbackController extends Controller
@@ -16,6 +17,11 @@ class AuthCallbackController extends Controller
         $user = $socialiteService
             ->forService($service)
             ->create(Socialite::driver($service)->user());
+
+        if ($user->wasRecentlyCreated) {
+            info('user was create');
+            event(new Registered($user));
+        }
 
         auth()->login($user);
 
